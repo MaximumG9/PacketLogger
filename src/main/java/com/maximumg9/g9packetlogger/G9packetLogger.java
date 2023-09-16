dcc2dca922e720d188336f9d697d4b2c6481909f
MaximumG9 <maximeguy9@gmail.com>
2023-09-16 10:41:54 -0700

    Initial commit

---
+++ src/main/java/com/maximumg9/g9packetlogger/G9packetLogger.java
@@ -0,0 +1,13 @@
+package com.maximumg9.g9packetlogger;
+
+import net.fabricmc.api.ModInitializer;
+
+public class G9packetLogger implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/maximumg9/g9packetlogger/client/G9packetLoggerClient.java
@@ -0,0 +1,13 @@
+package com.maximumg9.g9packetlogger.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class G9packetLoggerClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
